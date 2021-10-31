4c3bc274f1641af1c03befc68174c785ebf0cf1b
Sherin Cherian <39821938+smcck222@users.noreply.github.com>
2021-10-31 11:26:39 +0000

    Update classify_rpi.py

--- classify_rpi.py
+++ classify_rpi.py
@@ -69,7 +69,7 @@ def main():
   for x in os.listdir(args.captcha_dir):
     raw_data = cv2.imread(os.path.join(args.captcha_dir, x))
     rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
-    image = numpy.array(rgb_data) / 255.0
+    image = np.array(rgb_data) / 255.0
     (c, h, w) = image.shape
     image = image.reshape([-1, c, h, w])
     
